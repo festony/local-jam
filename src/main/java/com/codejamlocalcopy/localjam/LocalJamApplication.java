6c4614454efa2bdbf61f8880550e8da1ca38b3be
Leo Liang <leo.liang@fairfaxmedia.com.au>
2018-02-22 15:53:15 +1100

    Init commit

---
+++ src/main/java/com/codejamlocalcopy/localjam/LocalJamApplication.java
@@ -0,0 +1,12 @@
+package com.codejamlocalcopy.localjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalJamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalJamApplication.class, args);
+	}
+}
